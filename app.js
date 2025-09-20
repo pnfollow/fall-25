9ed4903ffdfd15e0cfa2c784e037a2063d88f6cb
pnfollow <paigefollowill.robinson@gmail.com>
2025-09-19 23:20:48 -0500

    4.5 Assignment

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-var life = 100; // Global
+const name = "Paige" ;
 
-life = life - 10; // Number
+const age = 20;
 
-const pi = 3.14 ;
-let name = "Dev Ed"; // String
-let answer = 'Yes I am!'; // Block 
+if(age > 16){
+    console.log("You may continue with the application.");
+} else if (age < 14) {
+    console.log("You are way too young to apply.");
+} else {
+    console.log("You are not old enough to apply.");
+}
 
-const heart = ["Black outline", "transparent center"];// Object
-heart.color = "red center";
+console.log(`Hello my name is ${name} I am ${age} years old and I am interested in this position.`);
 
+console.log(name + age);
 
-console.log(life) ;
+let text;
+switch (new Date().getDay()) {
+  case 6:
+    text = "Todays date is Saturday.";
+    break;
+  case 0:
+    text = "Todays date is Sunday.";
+    break;
+  default:
+    text = "Looking forward to the Weekend.";
+}
+
+console.log(text)
+
+const coin1=1;
+const coin2=2;
+
+if (coin1 === 1 && coin2 === 1) {
+    console.log("You rolled heads twice in a row!");
+} else {
+    console.log("You lost.")
+}
+
+if (coin1 === 1 || coin2 === 1) {
+    console.log("You win!");
+} else {
+    console.log("You lost.")
+}
