dde9de91c5f5dba36fbff18481631f307b31e0a8
pnfollow <paigefollowill.robinson@gmail.com>
2025-10-10 23:32:26 -0500

    6.4 Assignment

--- app.js
+++ app.js
@@ -1,44 +1,18 @@
-const name = "Paige" ;
+const user = {
+    name: 'Paige',
+    dayOfWeek: 'Saturday',
+    schedule: ['wake up', 'get ready', 'clean house', 'eat', 'clean car'],
+    dailySchedule: function() {
+    return this.dayOfWeek + " " + this.schedule;
+  }
+};
 
-const age = 20;
+user.schedule.push('go to store');
 
-if(age > 16){
-    console.log("You may continue with the application.");
-} else if (age < 14) {
-    console.log("You are way too young to apply.");
-} else {
-    console.log("You are not old enough to apply.");
-}
+console.log(user.schedule);
 
-console.log(`Hello my name is ${name} I am ${age} years old and I am interested in this position.`);
+console.log(user.schedule[4]);
 
-console.log(name + age);
+console.log(user.schedule.indexOf('get ready'));
 
-let text;
-switch (new Date().getDay()) {
-  case 6:
-    text = "Todays date is Saturday.";
-    break;
-  case 0:
-    text = "Todays date is Sunday.";
-    break;
-  default:
-    text = "Looking forward to the Weekend.";
-}
-
-console.log(text)
-
-const coin1=1;
-const coin2=2;
-
-if (coin1 === 1 && coin2 === 1) {
-    console.log("You rolled heads twice in a row!");
-} else {
-    console.log("You lost.")
-}
-
-if (coin1 === 1 || coin2 === 1) {
-    console.log("You win!");
-} else {
-    console.log("You lost.")
-}
+console.log(user.dailySchedule())
